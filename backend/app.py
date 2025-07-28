0d2dce97a4db06acc0522fa02783c5cc970c6f8f
abdullahhas <abdullah312003@gmail.com>
2025-07-29 01:09:17 +0500

    making the api and testing on postman and use proba for accurate prediction and solve the problem of always predicting not spam

--- backend/app.py
+++ backend/app.py
@@ -1,12 +1,13 @@
-from flask import Flask , request , jsonify
-from utils import clean_txt 
-import pickle , nltk
+from flask import Flask, request, jsonify
+from utils import clean_txt
+import joblib  
+import nltk
 
 nltk.download("stopwords")
 
 
-model = pickle.load(open('data/best_naive_bayes_model.pkl' , 'rb'))
-vectorizer = pickle.load(open('data/tfidf_vectorizer.pkl' , 'rb'))
+model = joblib.load('data/best_naive_bayes_model.pkl')
+vectorizer = joblib.load('data/tfidf_vectorizer.pkl')
 
 app = Flask(__name__)
 
@@ -14,19 +15,21 @@ app = Flask(__name__)
 def hello():
     return "hello"
 
-
-@app.route('/predict' , methods=['POST'])
+@app.route('/predict', methods=['POST'])
 def predict():
     data = request.get_json()
     msg = data['message']
 
     cleaned = clean_txt(msg)
     vector_input = vectorizer.transform([cleaned])
-    prediction = model.predict(vector_input)[0]
+    probs = model.predict_proba(vector_input)[0]
+    spam_prob = probs[1]  # index 1 is probability for class '1' (spam)
 
-    return jsonify({'prediction': 'Spam' if prediction == 1 else 'Not Spam'})
-
+    return jsonify({
+    'prediction': 'Spam' if spam_prob > 0.5 else 'Not Spam',
+    'spam_probability': round(spam_prob, 3)
+})
 
 
 if __name__ == "__main__":
-    app.run(debug = True)
+    app.run(debug=True)
--- backend/utils.py
+++ backend/utils.py
@@ -3,8 +3,6 @@ import nltk
 from nltk.corpus import stopwords
 from nltk.stem import PorterStemmer
 
-
-
 stemmer = PorterStemmer()
 stop_words = set(stopwords.words('english'))
 
@@ -12,7 +10,7 @@ def clean_txt(text):
     text = text.lower()                                           # Lowercase
     text = re.sub(r'http\S+|www\S+|https\S+', '', text)           # Remove links
     text = re.sub(r'\@\w+|\#', '', text)                          # Remove @mentions and hashtags
-    text = re.sub(r'[^a-zA-Z\s]', '', text)                       # Remove numbers/punctuation
+    text = re.sub(r'[^a-zA-Z]', ' ', text)                        # Keep only alphabets, replace others with space
     words = text.split()
-    words = [word for word in words if word not in stop_words]   # Remove stopwords
+    words = [stemmer.stem(word) for word in words if word not in stop_words]  # Remove stopwords and apply stemming
     return ' '.join(words)
